25bff8d324eed5a859321dbceba7a513093999a3
yuyingzhang <yuyi0011@stud.kea.dk>
2023-04-25 14:54:31 +0200

    Codes for project

--- src/main/java/dk/kea/projectadm/Model/Project.java
+++ src/main/java/dk/kea/projectadm/Model/Project.java
@@ -1,4 +1,27 @@
 package dk.kea.projectadm.Model;
 
 public class Project {
+    private int id;
+    private String projectName;
+
+    public Project(int id, String projectName) {
+        this.id = id;
+        this.projectName = projectName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
 }
